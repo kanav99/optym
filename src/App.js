cd07cca2b57a072bfa4a5d3eb04f11b3f88c743a
Kanav Gupta <kanav0610@gmail.com>
2021-05-25 20:14:20 +0530

    leet frontend

--- src/App.js
+++ src/App.js
@@ -12,87 +12,224 @@ import {
   Td,
   Tbody,
   useColorModeValue,
+  Heading,
+  Thead,
+  Th,
+  Stack,
+  IconButton,
+  ButtonGroup,
+  Container,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Input,
 } from '@chakra-ui/react';
+
+import { mode } from '@chakra-ui/theme-tools';
+
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import NavBar from './NavBar';
-import ActivityCard from './ActivityCard';
+// import CallToActionWithAnnotation from './Hero';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { Logo } from './Logo';
+
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { useRef } from 'react';
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const myRef = useRef(null);
+  const executeScroll = () =>
+    myRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
   return (
     <ChakraProvider theme={theme}>
       <Box position="sticky" top={0}>
-        <NavBar />
+        <NavBar onPrimary={onOpen} onSecondary={executeScroll} />
       </Box>
-      {/* <ColorModeSwitcher /> */}
-      <Box>
-        <Grid templateColumns="repeat(5, 1fr)" gap={3}>
-          <GridItem w="100%" h={500}>
-            <Box
-              p={2}
-              borderRight={1}
-              borderStyle={'solid'}
-              borderColor={useColorModeValue('gray.200', 'gray.900')}
+      <Box textAlign="center">
+        <Container maxW={'3xl'}>
+          <Stack
+            as={Box}
+            textAlign={'center'}
+            spacing={{ base: 8, md: 14 }}
+            py={{ base: 20, md: 16 }}
+          >
+            <Heading
+              fontWeight={600}
+              fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+              lineHeight={'110%'}
             >
-              {/* <Box p={1}>
-                <Tag>Profile</Tag>
-              </Box> */}
-              {/* Profile Pic */}
-              <Box p={1} paddingTop={4} textAlign="center">
-                <Image
-                  src="https://bit.ly/sage-adebayo"
-                  alt="Segun Adebayo"
-                  borderRadius="full"
-                />
-              </Box>
-              {/* Information */}
-              <Box>
-                <Table variant="unstyled" size="md">
-                  <Tbody>
-                    <Tr>
-                      <Td>
-                        <Text as="b">Name</Text>
-                      </Td>
-                      <Td>Segun Adebayo</Td>
-                    </Tr>
-                    <Tr>
-                      <Td>
-                        <Text as="b">Balance</Text>
-                      </Td>
-                      <Td>420.69 ETH</Td>
-                    </Tr>
-                    <Tr>
-                      <Td>
-                        <Text as="b">Contests Hosted</Text>
-                      </Td>
-                      <Td>12</Td>
-                    </Tr>
-                    <Tr>
-                      <Td>
-                        <Text as="b">Solutions Submitted</Text>
-                      </Td>
-                      <Td>54</Td>
-                    </Tr>
-                  </Tbody>
-                </Table>
+              <Text as={'span'} color={'green.400'}>
+                Kanav Gupta
+              </Text>{' '}
+              is organizing a stonk war worth{' '}
+              <Text as={'span'} color={'green.400'}>
+                0.1 ETH
+              </Text>
+            </Heading>
+            <Text color={'gray.500'}>
+              Here's the deal - find the input to the function given below which
+              maximizes the output value and one with the largest output value
+              wins 0.1 ETH.
+            </Text>
+            <Box textAlign="left">
+              <SyntaxHighlighter language="javascript" style={docco}>
+                {'function challenge(i)\n{\n\treturn 42 * 69;\n}'}
+              </SyntaxHighlighter>
+            </Box>
+            <Box>
+              <Box
+                as="button"
+                colorScheme={'green'}
+                bg={'green.400'}
+                d="inline!"
+                p={3}
+                borderRadius={'0.375rem'}
+                //   rounded={'full'}
+                px={6}
+                _hover={{
+                  bg: 'green.500',
+                }}
+                onClick={onOpen}
+              >
+                <Text color={mode('white', 'black!')}>Submit a solution</Text>
               </Box>
             </Box>
-          </GridItem>
-          {/* Activity feed */}
-          <GridItem w="100%" h={500} colSpan={4}>
-            <Box p={2}>
-              <Text>Activity Feed</Text>
-              <ActivityCard />
-              <ActivityCard />
-              <ActivityCard />
-              <ActivityCard />
-              <ActivityCard />
-              <ActivityCard />
-              <ActivityCard />
-              <ActivityCard />
-            </Box>
-          </GridItem>
-        </Grid>
+          </Stack>
+        </Container>
+
+        {/* Leaderboard Table */}
+        <Heading ref={myRef}>Leaderboard</Heading>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th isNumeric>Rank</Th>
+              <Th>Name</Th>
+              <Th isNumeric>Input</Th>
+              <Th isNumeric>Output</Th>
+              <Th>Submission time</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td isNumeric>1</Td>
+              <Td>Kanav Gupta</Td>
+              <Td isNumeric>5</Td>
+              <Td isNumeric>2898</Td>
+              <Td>25 May 2021 17:40</Td>
+            </Tr>
+            <Tr>
+              <Td isNumeric>2</Td>
+              <Td>Kanav Gupta</Td>
+              <Td isNumeric>5</Td>
+              <Td isNumeric>2898</Td>
+              <Td>25 May 2021 17:40</Td>
+            </Tr>
+            <Tr>
+              <Td isNumeric>3</Td>
+              <Td>Kanav Gupta</Td>
+              <Td isNumeric>5</Td>
+              <Td isNumeric>2898</Td>
+              <Td>25 May 2021 17:40</Td>
+            </Tr>
+            <Tr>
+              <Td isNumeric>4</Td>
+              <Td>Kanav Gupta</Td>
+              <Td isNumeric>5</Td>
+              <Td isNumeric>2898</Td>
+              <Td>25 May 2021 17:40</Td>
+            </Tr>
+            <Tr>
+              <Td isNumeric>5</Td>
+              <Td>Kanav Gupta</Td>
+              <Td isNumeric>5</Td>
+              <Td isNumeric>2898</Td>
+              <Td>25 May 2021 17:40</Td>
+            </Tr>
+            <Tr>
+              <Td isNumeric>6</Td>
+              <Td>Kanav Gupta</Td>
+              <Td isNumeric>5</Td>
+              <Td isNumeric>2898</Td>
+              <Td>25 May 2021 17:40</Td>
+            </Tr>
+          </Tbody>
+        </Table>
       </Box>
+
+      {/* Footer */}
+      <Box
+        as="footer"
+        role="contentinfo"
+        mx="auto"
+        maxW="7xl"
+        py="12"
+        px={{ base: '4', md: '8' }}
+      >
+        <Stack>
+          <Stack
+            direction="row"
+            spacing="4"
+            align="center"
+            justify="space-between"
+          >
+            <Logo />
+            <ButtonGroup variant="ghost" color="gray.600">
+              <IconButton
+                as="a"
+                href="#"
+                aria-label="LinkedIn"
+                icon={<FaLinkedin fontSize="20px" />}
+              />
+              <IconButton
+                as="a"
+                href="#"
+                aria-label="GitHub"
+                icon={<FaGithub fontSize="20px" />}
+              />
+              <IconButton
+                as="a"
+                href="#"
+                aria-label="Twitter"
+                icon={<FaTwitter fontSize="20px" />}
+              />
+            </ButtonGroup>
+          </Stack>
+          <Text fontSize="sm" alignSelf={{ base: 'center', sm: 'start' }}>
+            &copy; {new Date().getFullYear()} Envelope, Inc. All rights
+            reserved.
+          </Text>
+        </Stack>
+      </Box>
+
+      {/* Submitting modal */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Submit a solution</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>Waiting for Metamask...{'\n'}</Text>
+            <br />
+            <Input placeholder="Enter your solution" size="md"></Input>
+          </ModalBody>
+
+          <ModalFooter>
+            {/* <Button variant="ghost">Secondary Action</Button> */}
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Submit
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </ChakraProvider>
   );
 }
--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -14,6 +14,7 @@ import {
   useColorModeValue,
   useBreakpointValue,
   useDisclosure,
+  propNames,
 } from '@chakra-ui/react';
 import {
   HamburgerIcon,
@@ -21,12 +22,19 @@ import {
   ChevronDownIcon,
   ChevronRightIcon,
 } from '@chakra-ui/icons';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Logo } from './Logo';
 
-export default function WithSubnavigation() {
+interface Prop {
+  onPrimary: () => void;
+  onSecondary: () => void;
+}
+
+export default function WithSubnavigation(prop: Prop) {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <Box>
+    <Box zIndex={10000}>
       <Flex
         bg={useColorModeValue('white', 'gray.800')}
         color={useColorModeValue('gray.600', 'white')}
@@ -35,7 +43,7 @@ export default function WithSubnavigation() {
         px={{ base: 4 }}
         borderBottom={1}
         borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.900')}
+        borderColor={useColorModeValue('gray.200', 'gray.700')}
         align={'center'}
       >
         <Flex
@@ -53,14 +61,15 @@ export default function WithSubnavigation() {
           />
         </Flex>
         <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-          <Text
+          {/* <Text
             as="em"
             textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
             fontFamily={'heading'}
             color={useColorModeValue('gray.800', 'white')}
           >
             Stonk Wars
-          </Text>
+          </Text> */}
+          <Logo />
 
           <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
             <DesktopNav />
@@ -73,28 +82,40 @@ export default function WithSubnavigation() {
           direction={'row'}
           spacing={6}
         >
+          <ColorModeSwitcher />
           <Button
             as={'a'}
             fontSize={'sm'}
             fontWeight={400}
             variant={'link'}
-            href={'#'}
+            onClick={prop.onSecondary}
           >
-            Sign Out
+            Leaderboard
           </Button>
           {/* <Button
             display={{ base: 'none', md: 'inline-flex' }}
             fontSize={'sm'}
             fontWeight={600}
-            color={'white'}
-            bg={'pink.400'}
+            // color={'white'}
+            bg={'green.400'}
             href={'#'}
             _hover={{
-              bg: 'pink.300',
+              bg: 'green.300',
             }}
+          > */}
+          <Button
+            colorScheme={'green'}
+            bg={'green.400'}
+            fontSize={'sm'}
+            fontWeight={600}
+            //   rounded={'full'}
+            _hover={{
+              bg: 'green.500',
+            }}
+            onClick={prop.onPrimary}
           >
-            Sign Up
-          </Button> */}
+            Submit a solution
+          </Button>
         </Stack>
       </Flex>
 
@@ -295,12 +316,8 @@ const NAV_ITEMS: Array<NavItem> = [
   //     },
   //   ],
   // },
-  {
-    label: 'See Open Contests',
-    href: '#',
-  },
-  {
-    label: 'Start an Auction',
-    href: '#',
-  },
+  // {
+  //   label: 'Leaderboard',
+  //   href: '#',
+  // },
 ];
